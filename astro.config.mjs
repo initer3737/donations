b2611b287160a04a760e5a99f42d00fb9e959640
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2026-06-03 01:21:54 +0700

    add svelte deployment github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site:"https://klarivika.github.io",
+  integrations: [
+    svelte({extensions:['.svelte']}), 
+    sitemap()
+],
+  vite:{
+    css:{
+        transformer:'postcss'
+    },
+
+    plugins: [tailwindcss()]
+  }
+});
