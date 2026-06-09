eb8319b8e824c2e3b9ef632fc0185db286d48cff
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2026-06-10 06:37:48 +0700

    hybrid

--- src/components/GetAllDataDonations.svelte
+++ src/components/GetAllDataDonations.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import { fetchDonationsData } from '../stores/donations_all_data';
-
+    export const prerender = false;
     onMount(() => {
         // Cukup panggil fungsi ini sekali, store akan otomatis terisi dan memicu karosel bergerak!
-        fetchDonationsData();
+        fetchDonationsData({});
     });
 </script>
