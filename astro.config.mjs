b097b59fe1b83e2bad34915df557d6ec4902a171
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2026-06-11 03:22:40 +0700

    try to fix tailwind css bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://klarivika.github.io/donations",
+  site: process.env.PUBLIC_SITE_URL ?? 'https://donations.pages.dev',
   integrations: [
     svelte({extensions:['.svelte']}), 
     sitemap(),
   ],
-   adapter: cloudflare(),
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
   vite:{
     css:{
       transformer:'postcss'
